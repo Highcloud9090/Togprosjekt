8abe92704445ffe4748bc986a980ffd28943e1d8
Mads Ivarson Gram <141741175+madsig@users.noreply.github.com>
2026-02-20 14:09:33 +0100

    La til ny mappe og en mqtt test kode
    Ny mappe med ekstra ting og tang
    Koden sender vær verdi fra MPU-en seperat så det kan filtreses gjennom MQTT. Litt kult men JSON er nokk en bedre løsning

---
+++ Eksperimentelle_løsninger/Individuell_mqtt_data.cpp
@@ -0,0 +1,160 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <MPU6050.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// prototypes
+void calibrateSensor(int n);
+void callback(char* topic, byte* payload, unsigned int length);
+void reconnect();
+
+// WiFi and MQTT
+const char* ssid = "NTNU-IOT";
+const char* password = "";
+const char* mqtt_server = "10.22.100.166"; //change!
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// MPU6050
+#define SDA_PIN 26
+#define SCL_PIN 25
+
+MPU6050 mpu(0x69);// software offsets
+float ax_offset = 0, ay_offset = 0, az_offset = 0;
+float gx_offset = 0, gy_offset = 0, gz_offset = 0;
+
+String msg;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  msg = "Serial started (" + String(millis()) + ')';
+  Serial.println(msg);
+
+  // WiFi and MQTT
+  Serial.println("Attempting WiFi connection...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  Serial.print("\nWiFi connected, ip address = ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  Serial.println("\nInitializing Wire");
+  Wire.begin(SDA_PIN, SCL_PIN);
+  while(!Serial);
+  Serial.println("Wire initialized");
+
+  // MPU
+  Serial.println("\nInitializing MPU6050");
+
+  Serial.println("MPU6050 Raw + Software Offset Test");
+
+  mpu.initialize();
+  delay(500);
+  mpu.setSleepEnabled(false);
+
+  Serial.println("MPU6050 started");
+  delay(2000);
+
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6050 not connected!");
+    //while (1); delay(10);
+  }
+
+  // Sett full-scale range
+  mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);   // ±2g
+  mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_1000);  // ±1000°/s
+
+  Serial.println("Calibrating...");
+
+  calibrateSensor(2500); // 2500 tries
+  Serial.println("Calibration done!");
+}
+
+void loop() {
+  if (!client.connected()) reconnect();
+  client.loop();
+  static unsigned long last = 0;
+  if (millis() - last > 1000) {
+    last = millis();
+
+    int16_t ax_raw, ay_raw, az_raw, gx_raw, gy_raw, gz_raw;
+    mpu.getMotion6(&ax_raw, &ay_raw, &az_raw, &gx_raw, &gy_raw, &gz_raw);
+
+    float axs2 = ax_raw - ax_offset;
+    float ays2 = ay_raw - ay_offset;
+    float azs2 = az_raw - az_offset;
+
+    float gxdps = gx_raw - gx_offset;
+    float gydps = gy_raw - gy_offset;
+    float gzdps = gz_raw - gz_offset;
+
+    client.publish("devices/esp32-1/accel/x", String(axs2).c_str());
+    client.publish("devices/esp32-1/accel/y", String(ays2).c_str());
+    client.publish("devices/esp32-1/accel/z", String(azs2).c_str());
+    client.publish("devices/esp32-1/gyro/x",  String(gxdps).c_str());
+    client.publish("devices/esp32-1/gyro/y",  String(gydps).c_str());
+    client.publish("devices/esp32-1/gyro/z",  String(gzdps).c_str());
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (unsigned int i = 0; i < length; i++) Serial.print((char)payload[i]);
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("esp32-client")) {
+      Serial.println("connected");
+      client.subscribe("other/topic");
+      client.publish("esp32/topic","hello from esp32");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void calibrateSensor(int n) {
+  long ax_sum = 0, ay_sum = 0, az_sum = 0;
+  long gx_sum = 0, gy_sum = 0, gz_sum = 0;
+
+  int16_t ax, ay, az, gx, gy, gz;
+
+  for (int i = 0; i < n; i++) {
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    ax_sum += ax;
+    ay_sum += ay;
+    az_sum += az;
+    gx_sum += gx;
+    gy_sum += gy;
+    gz_sum += gz;
+    delay(5);
+  }
+
+  ax_offset = ax_sum / (float)n;
+  ay_offset = ay_sum / (float)n;
+  az_offset = (az_sum / (float)n) - 16384.0; // remove 1g from Z
+  gx_offset = gx_sum / (float)n;
+  gy_offset = gy_sum / (float)n;
+  gz_offset = gz_sum / (float)n;
+
+  Serial.println("Offsets beregnet:");
+  Serial.print("Accel X: "); Serial.print(ax_offset);
+  Serial.print("  Y: "); Serial.print(ay_offset);
+  Serial.print("  Z: "); Serial.println(az_offset);
+
+  Serial.print("Gyro X: "); Serial.print(gx_offset);
+  Serial.print("  Y: "); Serial.print(gy_offset);
+  Serial.print("  Z: "); Serial.println(gz_offset); 
+}
